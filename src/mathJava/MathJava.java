4bd87dfb949bb40fb0560967b4076604403e66d9
alexandergatien <alexandergatien@HHS-E116-20.Hasd.org>
2021-10-07 09:50:18 -0500

    Math Work

--- src/mathJava/MathJava.java
+++ src/mathJava/MathJava.java
@@ -4,6 +4,17 @@ public class MathJava {
 
 	public static void main(String[] args) {
 		
+		cuboidc(3,4,5);
+		cuboidt(3,4,5);
 	}
+public static void cuboidc (int h,int w,int l) {
 
+	System.out.println(2*h*(w+l));
+	
+	
+}
+public static void cuboidt (int h, int w, int l) {
+	System.out.println(2*(h*w+w*l+h*l));
+	
+}
 }
